2ef2ace0b0410564fe0ce94dcbe112b1c55754c6
kpedd <kpeddakotla@gmail.com>
2024-05-26 14:43:19 -0700

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,46 +1,25 @@
 import sys
-import os
 
-def find_command_in_path(command):
-        path_dirs = os.environ.get("PATH", "").split(os.pathsep)
-        for dir in path_dirs:
-            potential_path = os.path.join(dir, command)
-            if os.path.isfile(potential_path) and os.access(potential_path, os.X_OK):
-                return potential_path
-        return None
-    
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    #print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    #sys.stdout.write("$ ")
-    #sys.stdout.flush()
-    built = ["exit", "echo", "type"]
-    # Wait for user input
+    
+    built = ["echo", "exit", "type"]
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        
+
         if ans := input().strip():
             if ans == "exit 0":
                 sys.exit(0)
-            if ans[4:] in built:
-                sys.stdout.write(f"{ans[4:]} is a shell builtin\n")
-            elif "echo" in ans:
-                print(f"{ans[4:]}")
-            else:
-                print(f"{ans[4:]}: command not found")
-                paths = os.getenv("PATH").split(":")
-                for path in paths:
-                    if os.path.exists(f"{path}/{ans[4:]}"):
-                        sys.stdout.write(f"{ans[4:]} is {path}/{ans[4:]}\n")
-                        sys.stdout.flush()
-                        return
+            elif ans.startswith("type"):
+                command = ans[4:].strip()
+                if command in built:
+                    print(f"{command} is a shell builtin")
                 else:
-                    print(f"{ans[4:]}: command not found")
-
-
+                    print(f"/usr/local/{command}")
+            elif ans.startswith("echo"):
+                print(ans[4:])
+            else:
+                print(f"{ans}: command not found")
 
 if __name__ == "__main__":
     main()
