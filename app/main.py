7efe2ceef1ae3d09f2659737342a2f58efca549a
kpedd <kpeddakotla@gmail.com>
2024-05-26 15:31:51 -0700

    pass stage

--- app/main.py
+++ app/main.py
@@ -36,7 +36,7 @@ def main():
                 if os.path.exists(f"{ans[0]}"):
                     os.system(f"{command_args[0]} {' '.join(command_args[1:])}")
                 else:
-                    sys.stdout.write(f"{command}: command not found\n")
+                    sys.stdout.write(f"{ans[4:]}: command not found\n")
 
 if __name__ == "__main__":
     main()
